bcf6afe22130b480daacbbb7c89ccd61cdd166cd
MonzLy <75698415+Monzly17@users.noreply.github.com>
2020-12-23 21:00:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ async function starts() {
 				} catch {
 					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 				}
-				teks = `Halo @${num.split('@')[0]}\nSelamat datang di group kami *${mdata.subject}*`
+				teks = `Halo @${num.split('@')[0]}\nSelamat datang di group kami ya, semoga betah di group ini yaitu *${mdata.subject}*`
 				let buff = await getBuffer(ppimg)
 				client.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
 			} else if (anu.action == 'remove') {
